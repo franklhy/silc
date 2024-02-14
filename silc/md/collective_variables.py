8a4a091d974f8b057e1088d67974e6a1aad2a26b
Heyi Liang <frank_hy@hotmail.com>
2024-02-14 10:19:44 -0600

    New CV for distance between two molecules.

--- silc/md/collective_variables.py
+++ silc/md/collective_variables.py
@@ -1,4 +1,4 @@
-from pysages.colvars.core import ThreePointCV
+from pysages.colvars.core import TwoPointCV, ThreePointCV
 from pysages.colvars.coordinates import barycenter, distance
 from jax import numpy as np
 from jax.numpy import linalg
@@ -101,7 +101,7 @@ class CenterOfMassAngle(ThreePointCV):
     ----------
 
     indices:
-        This is a list of three set of indices. The first one corresponds to the bridge,
+        This is a list of three set of indices. The middle one corresponds to the bridge,
         and the rest correspond to the cores.
 
 
@@ -111,7 +111,7 @@ class CenterOfMassAngle(ThreePointCV):
     indices1 = [0, 1, 2, 3, 4, 5]    # atom indices of bridge
     indices2 = [10, 11, 12, 13, 14, 15]    # atom indices of one core
     indices3 = [20, 21, 22, 23, 24, 25]    # atom indices of another core
-    cv = DistanceSum([indices1, indices2, indices3])
+    cv = CenterOfMassAngle([indices2, indices1, indices3])
     """
 
     @property
@@ -127,3 +127,29 @@ def com_angle(r1, r2, r3):
     r = r3 - r2
     return np.arctan2(linalg.norm(np.cross(q, r)), np.dot(q, r))
 
+
+class DistancePBC(TwoPointCV):
+    """
+    Parameters
+    ----------
+
+    indcies:
+    """
+
+    def __init__(self, indices, box=[1., 1., 1.,]):
+        super().__init__(indices)
+        self.box = np.asarray(box)
+
+    @property
+    def function(self):
+        return lambda r1, r2: distance_pbc(r1, r2, self.box)
+
+
+def periodic(distance, box):
+    return np.mod(distance + box * 0.5, box) - 0.5 * box
+
+def distance_pbc(r1, r2, box):
+    r1 = barycenter(r1)
+    r2 = barycenter(r2)
+    dr = r1 - r2
+    return linalg.norm(periodic(dr, box))
